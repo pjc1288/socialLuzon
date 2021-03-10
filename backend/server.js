05a47ab5d4aba26f0c95a28c67439b39253b037a
pjc1288 <pjc.1288@gmail.com>
2021-03-10 12:10:02 +0100

    rutas y controladores api blog creados

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,17 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const mongoose =require ('mongoose')
+const mongoose = require('mongoose');
 require('dotenv').config();
+// bring routes
+const blogRoutes = require('./routes/blog');
 
 // app
 const app = express();
 
-//db
+// db
 mongoose
-    .connect(process.env.DATABASE, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true })
+    .connect(process.env.DATABASE_LOCAL, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
     .then(() => console.log('DB connected'));
 
 // middlewares
@@ -19,16 +21,11 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 // cors
-app.use(cors());
 if (process.env.NODE_ENV === 'development') {
     app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
-
-
-// routes
-app.get('/api', (req, res) => {
-    res.json({ time: Date().toString() });
-});
+// routes middleware
+app.use('/api', blogRoutes);
 
 // port
 const port = process.env.PORT || 8000;
