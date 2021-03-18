f09b670e25338adf18e1b3d871f118599f0468a5
pjc1288 <pjc.1288@gmail.com>
2021-03-18 21:59:23 +0100

    flex compartir

--- frontend/components/blog/Card.js
+++ frontend/components/blog/Card.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import renderHTML from 'react-render-html';
 import moment from 'moment';
 import { API } from '../../config';
-import { Paragraph } from '../../styles/globalStyles';
+import { Paragraph, colors } from '../../styles/globalStyles';
 import {BiShareAlt} from 'react-icons/bi'
 import {FiBell} from 'react-icons/fi'
 
@@ -31,18 +31,13 @@ const Card = ({ blog }) => {
 
 
 
-<div style={{display:'flex', flexDirection:'row', backgroundColor:'red',padding:'10px',   alignItems:'center'}}>
-                
+                <div style={{display:'flex', flexDirection:'row', backgroundColor:`${colors.gray1}`,padding:'10px',alignItems:'center', marginTop:'-10px', borderRadius:'5px'}}>
                         <Link href={`/blogs/${blog.slug}`}>
-                    
-                        <div style={{backgroundColor:'green', width:'400px', fontSize:'16px', textAlign:'left'}}>{blog.title}</div>
-                   
+                        <div style={{width:'400px', fontSize:'16px', textAlign:'left'}}>{blog.title}</div>
                          </Link>
-                         <div style={{backgroundColor:'green', width:'60px', lineHeight:'75px', textAlign:'center'}}><BiShareAlt/></div>
-<div style={{backgroundColor:'green', width:'60px', lineHeight:'75px', textAlign:'center'}}><FiBell/></div>
-
-                        
-</div>
+                         <div style={{width:'60px', lineHeight:'75px', textAlign:'center'}}><BiShareAlt/></div>
+                        <div style={{width:'60px', lineHeight:'75px', textAlign:'center'}}><FiBell/></div>                        
+                    </div>
 
 
                     </section>
---
+++ frontend/styles/blogCardsStyles.js
@@ -0,0 +1,12 @@
+import styled, {createGlobalStyle, css} from 'styled-components'
+
+
+export const ContainerBlogTitle = styled.div`
+display:'flex'; 
+flex-direction:'row'; 
+background-color:'red';
+padding:'10px';   
+align-items:'center'
+`;
+
+
